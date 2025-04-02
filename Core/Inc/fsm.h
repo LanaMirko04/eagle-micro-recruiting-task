05e83186ff5d836ebbd49a81659e6ba8356656fe
Mirko Lana <lana.mirko@icloud.com>
2025-04-02 18:17:49 +0200

    feat: add fine-state-machine definition and implementation

---
+++ Core/Inc/fsm.h
@@ -0,0 +1,37 @@
+#ifndef __FSM_H__
+#define __FSM_H__
+
+typedef enum {
+    FSM_STATE_INIT,
+    FSM_STATE_WAIT_REQ,
+    FSM_STATE_LISTENING,
+    FSM_STATE_PAUSE,
+    FSM_STATE_WARNING,
+    FSM_STATE_ERROR
+} fsm_state_t;
+
+typedef enum {
+    FSM_EVENT_NONE,
+    FSM_EVENT_BTN_PRESSED,
+    FSM_EVENT_WARNING,
+    FSM_EVENT_ERROR
+} fsm_event_t;
+
+/**
+ * @brief Initialize the FSM.
+ */
+void fsm_init(void);
+
+/**
+ * @brief Get the current state of the FSM.
+ * @return The current state.
+ */
+fsm_state_t fsm_get_state(void);
+
+/**
+ * @brief che porcoddio ne so
+ * @param event dio can leggi
+ */
+void fsm_update(fsm_event_t event);
+
+#endif // __FSM_H__
